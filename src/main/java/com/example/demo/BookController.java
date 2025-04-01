287998f84b881dc35b96a1d4204ceb7e7a091c12
arcyro <arek@arcyro.pl>
2025-04-01 12:09:57 +0200

    first commit

---
+++ src/main/java/com/example/demo/BookController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/list")
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "book/list";
+
+    }
+}
